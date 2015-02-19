d348892a1c1f37faaf38049bae34aa46c3f0aeed
Arnaud TANGUY <arn.tanguy@gmail.com>
2015-02-19 11:25:32 +0100

    Install FindICP.cmake file

---
+++ cmake/FindICP.cmake
@@ -0,0 +1,6 @@
+###########################################################
+###                         ICPConfig		###
+###########################################################
+
+set(ICP_INCLUDE_DIRS /usr/local/include/icp/)
+set(ICP_LIBRARIES /usr/local/lib/libicp.so)
